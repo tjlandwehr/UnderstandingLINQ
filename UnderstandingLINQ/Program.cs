448062cee68899c9c72d0f8ddfce86140d24cc0f
Timothy Landwehr <tjlandwehr@gmail.com>
2018-03-08 14:30:17 -0600

    Looked at using different LINQ queries and methods.

--- UnderstandingLINQ/Program.cs
+++ UnderstandingLINQ/Program.cs
@@ -19,9 +19,57 @@ namespace UnderstandingLINQ
             };
 
             // LINQ query
+            /*
+            var bmws = from car in myCars
+                       where car.Make == "BMW"
+                       && car.Year == 2010
+                       select car;
+            */
+
+            /*
+            var orderedCars = from car in myCars
+                              orderby car.Year descending
+                              select car;
+            */
 
             // LINQ method
+            //var bmws = myCars.Where(p => p.Make == "BMW" && p.Year == 2010);
 
+            //var orderedCars = myCars.OrderByDescending(p => p.Year);
+
+            /*
+            var firstBMW = myCars.OrderByDescending(p => p.Year).First(p => p.Make == "BMW");
+            Console.WriteLine(firstBMW.VIN);
+            */
+
+            //Console.WriteLine(myCars.TrueForAll(p => p.Year > 2007));
+
+            //myCars.ForEach(p => p.StickerPrice -= 3000);
+            //myCars.ForEach(p => Console.WriteLine("{0} {1:C}", p.VIN, p.StickerPrice));
+
+            //Console.WriteLine(myCars.Exists(p => p.Model == "745li"));
+
+            //Console.WriteLine(myCars.Sum(p => p.StickerPrice));
+
+            /*
+            foreach (var car in orderedCars)
+            {
+                Console.WriteLine("{0} {1} {2}", car.Year, car.Model, car.VIN);
+            }
+            */
+
+            Console.WriteLine(myCars.GetType());
+            var orderedCars = myCars.OrderByDescending(p => p.Year);
+            Console.WriteLine(orderedCars.GetType());
+
+            var bmws = myCars.Where(p => p.Make == "BMW" && p.Year == 2010);
+            Console.WriteLine(bmws.GetType());
+
+            var newCars = from car in myCars
+                       where car.Make == "BMW"
+                       && car.Year == 2010
+                       select new { car.Make, car.Model };
+            Console.WriteLine(newCars.GetType());
 
             Console.ReadLine();
         }
